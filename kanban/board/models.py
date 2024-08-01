298c9a676801b373813cf40287c264baeb8d2f3d
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-08-01 10:50:22 +0200

    first commit

---
+++ kanban/board/models.py
@@ -0,0 +1,15 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    mail = models.CharField(max_length=50)
+
