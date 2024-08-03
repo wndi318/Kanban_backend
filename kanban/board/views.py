c760f70cf5d1ea157cb9b2a7b827544f22e2e662
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-08-03 12:03:54 +0200

    added auth token

--- kanban/board/views.py
+++ kanban/board/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from rest_framework import permissions, viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from .serializers import ContactSerializer, TaskSerializer
 from .models import Contact, Task
+
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('created_at')
--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from board.views import LoginView
 from board import views
 
 router = routers.DefaultRouter()
@@ -9,5 +10,6 @@ router.register(r'contact', views.ContactViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', LoginView.as_view()),
     path('admin/', admin.site.urls),
 ]
