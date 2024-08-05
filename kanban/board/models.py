a3791353d923afb6e168473e98cc91eaa38f0b6a
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-08-05 20:14:49 +0200

    updated task model

--- kanban/board/models.py
+++ kanban/board/models.py
@@ -2,15 +2,6 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
-class Task(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200, blank=True)
-    created_at = models.DateField(default=datetime.date.today)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'({self.id}) {self.title}'
-
 class Contact(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
@@ -19,3 +10,22 @@ class Contact(models.Model):
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
 
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    status = models.CharField(max_length=15, choices=[
+        ('todo', 'Todo'),
+        ('inProgress', 'In Progress'),
+        ('awaitFeedback', 'Await Feedback')
+    ], default='todo')
+    priority = models.CharField(max_length=6, choices=[
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low')
+    ], default='medium')
+    due_date = models.DateField(default=datetime.date.today)
+    assigned_to = models.ManyToManyField('Contact', related_name='assigned_tasks')
+    created_at = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.title
